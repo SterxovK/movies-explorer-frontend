ef5862e9c2da347c9459238b444d0943d92f335d
Kostya Sterkhov <sterxovk@mail.ru>
2022-08-27 21:04:24 +0400

    fix Nav

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -2,8 +2,6 @@ import React from 'react';
 
 import Promo from '../Promo/Promo';
 
-import NavTab from '../NavTab/NavTab';
-
 import AboutProject from '../AboutProject/AboutProject';
 
 import Techs from '../Techs/Techs';
